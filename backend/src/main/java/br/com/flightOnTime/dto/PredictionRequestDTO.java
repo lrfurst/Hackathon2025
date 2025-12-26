7d3f5aad6e53ed1100da6b39d3ea7f402310d3c9
crrsj <crrsj1@gmail.com>
2025-12-26 15:05:19 -0300

    Adicionando o PredictionService, e as validações

--- backend/src/main/java/br/com/flightOnTime/dto/PredictionRequestDTO.java
+++ backend/src/main/java/br/com/flightOnTime/dto/PredictionRequestDTO.java
@@ -1,16 +1,23 @@
 package br.com.flightOnTime.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class PredictionRequestDTO {
-
+    @NotBlank(message = "O campo 'companhia' é obrigatório.")
     private String companhia;
+    @NotBlank(message = "O campo 'origem' é obrigatório.")
     private String origem;
+    @NotBlank(message = "O campo 'destino' é obrigatório.")
     private String destino;
+    @NotNull(message = "A 'data_partida' é obrigatória.")
     private String data_partida;
+    @Positive(message = "A distância deve ser um valor positivo.")
     private int distancia_km;
 
 }
--- backend/src/main/java/br/com/flightOnTime/dto/VooRequisicao.java
+++
@@ -1,9 +0,0 @@
-package br.com.flightOnTime.dto;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-public class VooRequisicao {
-}
