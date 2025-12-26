f135dca5e725ece8688b3a1bfebec181ee5bb6a7
crrsj <crrsj1@gmail.com>
2025-12-26 15:05:19 -0300

    Adicionando o PredictionService, e as validações

---
+++ backend/src/main/java/br/com/flightOnTime/service/PredictionServie.java
@@ -0,0 +1,44 @@
+package br.com.flightOnTime.service;
+
+
+import br.com.flightOnTime.dto.PredictionRequestDTO;
+import br.com.flightOnTime.dto.PredictionResponseDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class PredictionServie {
+    // Endereço do microserviço Python (porta 5000)
+    private final String PYTHON_API_URL = "http://localhost:5000"; // URL base
+    private final String PREDICT_ENDPOINT = "/predict_internal"; // Endpoint específico
+    private final WebClient webClient;
+
+    public PredictionServie(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder
+                .baseUrl(PYTHON_API_URL)
+                .build();
+    }
+    // Construtor: Spring injeta o WebClient.Builder criado na WebClientConfig
+
+    public PredictionResponseDTO getPrediction(PredictionRequestDTO request) {
+        // Validação Simples antes de enviar
+        //remover esta linha ao criar o controller
+        if (request.getOrigem() == null || request.getData_partida() == null) {
+            throw new IllegalArgumentException("Os campos 'origem' e 'data_partida' são obrigatórios.");
+        }
+
+        // 1. Chama a API Python (POST para http://localhost:5000/predict_internal)
+        return webClient.post()
+                .uri(PREDICT_ENDPOINT)
+                .bodyValue(request) // Envia o objeto FlightRequest como corpo JSON
+                .retrieve()
+                // 2. Tratamento de Erros da Chamada HTTP (4xx e 5xx)
+                .onStatus(status -> status.is4xxClientError() || status.is5xxServerError(),
+                        response -> Mono.error(new RuntimeException("Erro ao chamar o serviço de previsão Python: Código " + response.statusCode())))
+
+                // 3. Mapeia a Resposta JSON para o objeto Java PredictionResponse
+                .bodyToMono(PredictionResponseDTO.class)
+                .block(); // Bloqueia para obter o resultado (simples para MVP)
+    }
+}
