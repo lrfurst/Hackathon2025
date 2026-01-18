6ea8cbbc3c8f5015594118483c0c05792b1fc65a
Sulamita Mendes <sulamitamendes2016@email.com>
2026-01-17 21:37:29 -0300

    Alterações nos dados para a integração

--- backend/src/main/java/br/com/flightOnTime/dto/PredictionRequestDTO.java
+++ backend/src/main/java/br/com/flightOnTime/dto/PredictionRequestDTO.java
@@ -3,6 +3,7 @@ package br.com.flightOnTime.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,22 +11,32 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PredictionRequestDTO {
 
-    @NotBlank(message = "não pode estar em branco.")
+    @NotBlank(message = "O campo 'companhia' é obrigatório.")
     private String companhia;
-    @NotBlank(message = "não pode estar em branco.")
+
+    @NotBlank(message = "O campo 'origem' é obrigatório.")
+    @JsonProperty("aeroporto_origem")
     private String origem;
-    @NotBlank(message = "não pode estar em branco.")
+
+    @NotBlank(message = "O campo 'destino' é obrigatório.")
+    @JsonProperty("aeroporto_destino")
     private String destino;
-    @NotBlank(message = "não pode estar em branco.")
-    private String data_partida;
-    @NotBlank(message = "não pode estar em branco.")
-    private int distancia_km;
+
+    @NotBlank
+    @JsonProperty("data_partida")
+    private String dataPartida;
+
+    @Positive(message = "A distância deve ser um valor positivo.")
+    @JsonProperty("distancia")
+    private Integer distanciaKm;
+
     @NotBlank(message = "O campo 'hora_dia' (manha, tarde, noite) é obrigatório.")
-    @JsonProperty("hora_dia")
+    @JsonProperty("hora_partida")
     private String horaDia;
+
     @NotNull(message = "O campo 'dia_semana' (1-7) é obrigatório.")
     @JsonProperty("dia_semana")
-    private int diaSemana;
+    private Integer diaSemana;
 
 
 }
--- backend/src/main/java/br/com/flightOnTime/dto/PredictionResponseDTO.java
+++ backend/src/main/java/br/com/flightOnTime/dto/PredictionResponseDTO.java
@@ -1,11 +1,22 @@
 package br.com.flightOnTime.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class PredictionResponseDTO {
-    private String previsao;
-    private double probabilidade;
+    @JsonProperty("resultadoPrevisao")
+    @JsonAlias("prediction")
+    private Integer previsao;
+
+    @JsonProperty("probabilidade")
+    @JsonAlias("probability")
+    private Double probabilidade;
+
+    @JsonProperty("custoEvitado")
+    @JsonAlias("avoided_cost")
+    private Double custoEvitado;
 }
